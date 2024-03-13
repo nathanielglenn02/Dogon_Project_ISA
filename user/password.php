bf4dade2009461ac26527797ea0603634ba439fa
Duarss <s160422035@student.ubaya.ac.id>
2024-03-14 00:11:31 +0700

    edit video 10 done by darius

--- user/password.php
+++ user/password.php
@@ -12,6 +12,34 @@ require_once "../template/header.php";
 require_once "../template/navbar.php";
 require_once "../template/sidebar.php";
 
+if (isset($_GET['msg'])) {
+    $msg = $_GET['msg'];
+} else {
+    $msg = '';
+}
+
+$alert = "";
+if ($msg == 'updated') {
+    $alert = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <i class="fa-solid fa-circle-check"></i> Ganti Password berhasil..
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+}
+
+if ($msg == 'err1') {
+    $alert = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <i class="fa-solid fa-circle-xmark"></i> Ganti Password gagal, konfirmasi password Anda tidak sesuai..
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+}
+
+if ($msg == 'err2') {
+    $alert = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <i class="fa-solid fa-circle-xmark"></i> Ganti Password gagal, password lama tidak sesuai..
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+}
+
 ?>
 
 
@@ -23,19 +51,40 @@ require_once "../template/sidebar.php";
                 <li class="breadcrumb-item"><a href="../index.php" style="text-decoration: none; color:black;">Home</a></li>
                 <li class="breadcrumb-item active">Ganti Password</li>
             </ol>
-
-            <div class="card">
-                <div class="card-header">
-                    <span class="h5 my-2"><i class="fa-solid fa-pen-to-square"></i>Ganti Password</span>
-                    <button type="submit" name="simpan" class="btn btn-primary float-end"><i class="fa-solid fa-floppy-disk"></i> Simpan</button>
-                    <button type="reset" name="reset" class="btn btn-danger float-end me-1"><i class="fa-solid fa-xmark"></i> Simpan</button>
+            <form action="proses-password.php" method="POST">
+                <?php
+                if ($msg != '') {
+                    echo $alert;
+                }
+                ?>
+                <div class="card">
+                    <div class="card-header">
+                        <span class="h5 my-2"><i class="fa-solid fa-pen-to-square"></i>Ganti Password</span>
+                        <button type="submit" name="simpan" class="btn btn-primary float-end"><i class="fa-solid fa-floppy-disk"></i>Simpan</button>
+                        <button type="reset" name="reset" class="btn btn-danger float-end me-1"><i class="fa-solid fa-xmark"></i>Reset</button>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3 row">
+                            <div class="col-7">
+                                <label for="currentPass" class="form-label">Password Lama</label>
+                                <input type="password" class="form-control" id="curPass" name="curPass" placeholder="Masukkan password lama Anda disini" required>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <div class="col-7">
+                                <label for="newPass" class="form-label">Password Baru</label>
+                                <input type="password" class="form-control" id="newPass" name="newPass" minlength="4" maxlength="20" placeholder="Masukkan password baru Anda disini" required>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <div class="col-7">
+                                <label for="confirmPass" class="form-label">Konfirmasi Password</label>
+                                <input type="password" class="form-control" id="confirmPass" name="confirmPass" placeholder="Ulangi password baru Anda" required>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+            </form>
         </div>
     </main>
 </div>
