a8b62ac7824f40694c43d67ccdd72760a05f0a87
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-04-16 20:50:30 +0700

    guru SELESAI GLENN

--- guru/edit-guru.php
+++ guru/edit-guru.php
@@ -33,17 +33,19 @@ $data = mysqli_fetch_array($queryGuru);
                 <div class="card">
                     <div class="card-header">
                         <span class="h2 my-2"><i class="fa-solid fa-pen-to-square"></i> Update Guru</span>
-                        <button type="submit" name="update" class="btn btn-primary float-end"><i class="fa-solid fa-floppy-disk"></i> Update</button>
+                        <button type="submit" name="update" class="btn btn-primary float-end"><i
+                                class="fa-solid fa-floppy-disk"></i> Update</button>
                     </div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-8">
-                                <input type="hidden" name="nip" value="<?= $data['id'] ?>">
+                                <input type="hidden" name="id" value="<?= $data['id'] ?>">
                                 <div class="mb-3 row">
                                     <label for="nip" class="col-sm-2 col-form-label">NIP</label>
                                     <label for="nip" class="col-sm-1 col-form-label">:</label>
                                     <div class="col-sm-9" style="margin-left: -50px;">
-                                        <input type="text" name="nip" pattern="[0-9]{18,}" title="minimal 18 angka" class="form-control ps-2
+                                        <input type="text" name="nip" pattern="[0-9]{18,}" title="minimal 18 angka"
+                                            class="form-control ps-2
                                     border-0 border-bottom" value="<?= $data['nip'] ?>" required>
                                     </div>
                                 </div>
@@ -59,7 +61,8 @@ $data = mysqli_fetch_array($queryGuru);
                                     <label for="telepon" class="col-sm-2 col-form-label">Telepon</label>
                                     <label for="" class="col-sm-1 col-form-label">:</label>
                                     <div class="col-sm-9" style="margin-left: -50px;">
-                                        <input type="tel" name="telepon" pattern="[0-9]{5,}" title="minimal 5 angka" class="form-control ps-2
+                                        <input type="tel" name="telepon" pattern="[0-9]{5,}" title="minimal 5 angka"
+                                            class="form-control ps-2
                                     border-0 border-bottom" value="<?= $data['telepon'] ?>" required>
                                     </div>
                                 </div>
@@ -67,16 +70,17 @@ $data = mysqli_fetch_array($queryGuru);
                                     <label for="agama" class="col-sm-2 col-form-label">Agama</label>
                                     <label for="" class="col-sm-1 col-form-label">:</label>
                                     <div class="col-sm-9" style="margin-left: -50px;">
-                                        <select name="agama" id="agama" class="form-select border-0 border-bottom" required>
+                                        <select name="agama" id="agama" class="form-select border-0 border-bottom"
+                                            required>
                                             <?php
                                             $agama = ['Islam', 'Kristen', 'Katolik', 'Hindu', 'Budha'];
                                             foreach ($agama as $rlg) {
                                                 if ($data['agama'] == $rlg) { ?>
-                                                    <option value="<?= $rlg ?>" selected><?= $rlg ?>
-                                                    </option>
-                                                <?php } else { ?>
-                                                    <option value="<?= $rlg ?>"><?= $rlg ?>
-                                                    </option>
+                                            <option value="<?= $rlg ?>" selected><?= $rlg ?>
+                                            </option>
+                                            <?php } else { ?>
+                                            <option value="<?= $rlg ?>"><?= $rlg ?>
+                                            </option>
                                             <?php
                                                 }
                                             }
@@ -88,15 +92,18 @@ $data = mysqli_fetch_array($queryGuru);
                                     <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
                                     <label for="" class="col-sm-1 col-form-label">:</label>
                                     <div class="col-sm-9" style="margin-left: -50px;">
-                                        <textarea name="alamat" id="alamat" cols="30" rows="3" class="form-control" required><?= $data['alamat'] ?></textarea>
+                                        <textarea name="alamat" id="alamat" cols="30" rows="3" class="form-control"
+                                            required><?= $data['alamat'] ?></textarea>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-4 text-center px-5">
                                 <input type="hidden" name="fotoLama" value="<?= $data['foto'] ?>">
-                                <img src="../asset/image/<?= $data['foto'] ?>" class="mb-3 rounded-circle" width="40%" alt="">
+                                <img src="../asset/image/<?= $data['foto'] ?>" class="mb-3 rounded-circle" width="40%"
+                                    alt="">
                                 <input type="file" name="image" class="form-control form-control-sm">
-                                <small class="text-secondary">Pilih foto PNG, JPG atau JPEG dengan ukuran maximal 1 MB</small>
+                                <small class="text-secondary">Pilih foto PNG, JPG atau JPEG dengan ukuran maximal 1
+                                    MB</small>
                                 <div><small class="text-secondary">width = height</small></div>
                             </div>
                         </div>
